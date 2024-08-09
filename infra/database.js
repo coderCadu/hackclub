ce5ac34f592f6b544a6db46102cb51855e858822
Carlos Silva <carlosedu.waldorf@gmail.com>
2024-08-09 19:20:58 -0300

    chore(env): Add  variables

--- infra/database.js
+++ infra/database.js
@@ -2,11 +2,11 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    user: "postgres",
-    host: "localhost",
-    database: "hackclub",
-    password: "password",
-    port: 5432,
+    user: process.env.POSTGRES_USER || "postgres",
+    password: process.env.POSTGRES_PASSWORD || "password",
+    host: process.env.POSTGRES_HOST || "localhost",
+    port: process.env.POSTGRES_PORT || 5432,
+    database: process.env.POSTGRES_DB || "hackclub",
   });
   await client.connect();
   const result = await client.query(queryObject);
