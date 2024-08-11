fdd75f694428c7dab5c8d29cad1091edae910b1b
Carlos Silva <carlosedu.waldorf@gmail.com>
2024-08-11 11:55:45 -0300

    chore(config): Add `jsconfig.json` and update archives

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function apiStatus(req, res) {
+import database from "infra/database";
+
+async function apiStatus(req, res) {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result.rows[0].sum);
   res.status(200).json({ status: "ok" });
 }
 
