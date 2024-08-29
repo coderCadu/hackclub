ee133366664a503c1e9ee65a2a1d3e7d175a8a0a
Carlos Silva <carlos.silva2@telefonica.com>
2024-08-29 12:28:39 -0300

    feat(migrations): Add migrationsRunner on `/migrations`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,7 +1,15 @@
 import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
 
-async function migrations(req, res) {
-  res.status(200).json([]);
+export default async function migrations(req, res) {
+  const migrations = await migrationRunner({
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    migrationsTable: "pgmigrations",
+    dir: join('infra', 'migrations'),
+    direction: "up",
+    verbose: true,
+  })
+
+  res.status(200).json(migrations);
 }
-
-export default migrations;
---
+++ tests/integration/api/v1/migrations/get.spec.js
@@ -0,0 +1,30 @@
+describe("api/v1/migrations", () => {
+  describe("When test the success of apiStatus method", () => {
+    let response;
+    let responseBody;
+
+    beforeEach(async () => {
+      jest.clearAllMocks();
+      response = await fetch("http://localhost:3000/api/v1/migrations");
+      responseBody = await response.json([]);
+    });
+
+    describe("Should return 200", () => {
+      it("And GET to /api/v1/migrations should return 200", async () => {
+        expect(response.status).toBe(200);
+      });
+    });
+
+    describe("Should have responseBody", () => {
+      it("And GET to /api/v1/migrations should return be defined", async () => {
+        expect(responseBody).toBeDefined();
+      });
+    });
+
+    describe("Should verify returned array", () => {
+      it("And GET to /api/v1/migrations should return an array", async () => {
+        expect(Array.isArray(responseBody)).toBe(true);
+      })
+    });
+  });
+});
