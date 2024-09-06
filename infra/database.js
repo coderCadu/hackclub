681808219a7da8b9bc366295bcc184be6b02fd71
Carlos Silva <carlosedu.waldorf@gmail.com>
2024-09-06 13:05:56 -0300

    chore: Remove default pass in database file

--- infra/database.js
+++ infra/database.js
@@ -6,11 +6,11 @@ async function query(queryObject) {
   let result;
 
   const client = new Client({
-    user: process.env.POSTGRES_USER || "postgres",
-    password: process.env.POSTGRES_PASSWORD || "password",
-    host: process.env.POSTGRES_HOST || "localhost",
-    port: process.env.POSTGRES_PORT || 5432,
-    database: process.env.POSTGRES_DB || "hackclub",
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
     ssl: getSSLValues(),
   });
 
@@ -30,7 +30,7 @@ async function databaseStatus() {
   const maxConnectionsResult = await this.query("SHOW max_connections;");
   const maxConnections = maxConnectionsResult.rows[0].max_connections;
 
-  const databaseName = process.env.POSTGRES_DB || "hackclub";
+  const databaseName = process.env.POSTGRES_DB;
   const openedCoonectionsResult = await this.query({
     text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
     values: [databaseName],
