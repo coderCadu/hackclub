25487f44746220da949798bafaa7520be5c68bb2
Carlos Silva <carlosedu.waldorf@gmail.com>
2024-09-09 09:27:03 -0300

    feat(api): Improve logic of routes

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,35 +3,39 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(req, res) {
-  const client = await database.getNewClient();
-  const defaultMigrationsOptions = {
-    dbClient: client,
-    dryRun: req.method === "GET" ? true : false,
-    migrationsTable: "pgmigrations",
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-  };
-
-  const migrations = await migrationRunner(defaultMigrationsOptions);
-
-  await client.end();
-
-  if (req.method === "GET") {
-    return res.status(200).json(migrations);
-  }
-
-  if (req.method === "POST") {
-    if (migrations.length > 0) {
-      return res.status(201).json(migrations);
-    }
-    return res.status(200).json(migrations);
-  }
-
   const allowedMethod = ["GET", "POST"];
-  if (!allowedMethod.contains(req.method)) {
-    return res.status(405).send("Method Not Allowed");
+  if (!allowedMethod.includes(req.method)) {
+    return res.status(405).json({ error: `Method ${req.method} Not Allowed` });
   }
 
-  return res.status(405).end();
+  let client;
+  try {
+    client = await database.getNewClient();
+    const defaultMigrationsOptions = {
+      dbClient: client,
+      dryRun: req.method === "GET" ? true : false,
+      migrationsTable: "pgmigrations",
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+    };
+
+    const migrations = await migrationRunner(defaultMigrationsOptions);
+
+    if (req.method === "GET") {
+      return res.status(200).json(migrations);
+    }
+
+    if (req.method === "POST") {
+      if (migrations.length > 0) {
+        return res.status(201).json(migrations);
+      }
+      return res.status(200).json(migrations);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
 }
