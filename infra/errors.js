2191b7b2985bd846bd2faa2a8c8481adee0115ee
Carlos Silva <carlos.silva2@telefonica.com>
2024-12-30 17:46:25 -0300

    feat: update migrations file

--- infra/errors.js
+++ infra/errors.js
@@ -1,9 +1,28 @@
-export class DatabaseError extends Error {
+export class DatabaseConnectionError extends Error {
   constructor({ cause }) {
     super("A database error occurred", { cause });
     this.name = "Database Error";
     this.message = "An error occurred while trying to connect to the database";
     this.action = "Drink some Coffee or Tea and try again later";
+    this.statusCode = "500";
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class DatabaseMigrationError extends Error {
+  constructor({ cause }) {
+    super("An error occurred when trying run migrations", { cause });
+    this.name = "Migration Error";
+    this.message = "An error occurred when trying run migrations";
+    this.action = "Please message the support team";
     this.statusCode = "500";
   }
 
